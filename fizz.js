a1a387bc92c4d1e6ac2ebba751f6db845a915c4f
st15 <stoyanova.mail@gmail.com>
2015-02-11 21:11:59 +0200

    second commit

--- fizz.js
+++ fizz.js
@@ -1,10 +1,10 @@
 function fizzBuzz() {
 	for (var i = 1; i <= 100; i++) {
 		if (i % 3 == 0) {
-			if (i % 5 == 0) { console.log( i + ": FizzBuzz"); }
-			else { console.log( i + ": Fizz"); }
+			if (i % 5 == 0) { console.log(i + ": FizzBuzz"); }
+			else { console.log(i + ": Fizz"); }
 		}
-		else if (i % 5 == 0) { console.log( i + ": Buzz"); }
+		else if (i % 5 == 0) { console.log(i + ": Buzz"); }
 		else { console.log(i); }
 	}
 }
