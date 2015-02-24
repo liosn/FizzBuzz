8faa32e782b44094a689bab35821bc48fc7fd39e
liosn <liosn@users.noreply.github.com>
2015-02-24 19:47:03 +0200

    Update fizz.js
    Reformatting

--- fizz.js
+++ fizz.js
@@ -1,11 +1,22 @@
 function fizzBuzz() {
 	for (var i = 1; i <= 100; i++) {
 		if (i % 3 == 0) {
-			if (i % 5 == 0) { console.log(i + ": FizzBuzz"); }
-			else { console.log(i + ": Fizz"); }
+			if (i % 5 == 0) { 
+				console.log(i + ": FizzBuzz"); 
+				
+			}
+			else { 
+				console.log(i + ": Fizz"); 
+				
+			}
 		}
-		else if (i % 5 == 0) { console.log(i + ": Buzz"); }
-		else { console.log(i); }
+		else if (i % 5 == 0) { 
+			console.log(i + ": Buzz"); 
+			
+		}
+		else { 
+			console.log(i); 
+		}
 	}
 }
 fizzBuzz();
